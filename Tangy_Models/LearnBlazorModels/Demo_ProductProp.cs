48a2d268e5d38dceae3d375a93d35e2dc569efbf
DeeJayJay <deejayjay@zoho.com>
2023-05-22 11:56:35 -0600

    Completes section 5

---
+++ Tangy_Models/LearnBlazorModels/Demo_ProductProp.cs
@@ -0,0 +1,9 @@
+﻿namespace Tangy_Models.LearnBlazorModels
+{
+    public class Demo_ProductProp
+    {
+        public int Id { get; set; }
+        public string Key { get; set; } = null!;
+        public string Value { get; set; } = null!;
+    }
+}
