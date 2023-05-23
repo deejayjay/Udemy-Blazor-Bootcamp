35562ebea3266c5941139ca2ecec5d2d3052c43e
DeeJayJay <deejayjay@zoho.com>
2023-05-22 21:22:09 -0600

    Finishes section 8

---
+++ Tangy_Business/Repository/CategoryRepository.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using Tangy_Business.Repository.IRepository;
+using Tangy_DataAccess;
+using Tangy_DataAccess.Data;
+using Tangy_Models;
+
+namespace Tangy_Business.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        #region Dependency Injection
+        
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+        public CategoryRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        } 
+
+        #endregion
+
+        public CategoryDTO Create(CategoryDTO objDTO)
+        {
+            // Converts CategoryDTO to Category using AutoMapper
+            var obj = _mapper.Map<CategoryDTO, Category>(objDTO);
+            obj.CreatedDate = DateTime.Now;
+            
+            var addedObj = _db.Categories!.Add(obj);
+            _db.SaveChanges();
+
+            // Converts Category to CategoryDTO using AutoMapper
+            return _mapper.Map<Category, CategoryDTO>(addedObj.Entity);
+        }
+
+        public int Delete(int id)
+        {
+            var obj = _db.Categories!.Find(id);
+
+            if (obj is not null)
+            {
+                _db.Categories!.Remove(obj);
+                return _db.SaveChanges();
+            }
+            return 0;
+        }
+
+        public CategoryDTO Get(int id)
+        {
+            var obj = _db.Categories!.Find(id);
+            
+            if (obj is not null) 
+            {
+                return _mapper.Map<Category, CategoryDTO>(obj);
+            }
+            return new CategoryDTO();
+        }
+
+        public IEnumerable<CategoryDTO> GetAll()
+        {
+            return _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryDTO>>(_db.Categories!);
+        }
+
+        public CategoryDTO Update(CategoryDTO objDTO)
+        {
+            var objFromDb = _db.Categories!.Find(objDTO.Id);
+
+            if (objFromDb is not null)
+            {
+                objFromDb.Name = objDTO.Name;
+                _db.Categories.Update(objFromDb);
+                _db.SaveChanges();
+                return _mapper.Map<Category, CategoryDTO>(objFromDb);
+            }
+
+            return objDTO;
+        }
+    }
+}
---
+++ Tangy_Business/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Tangy_Models;
+
+namespace Tangy_Business.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public CategoryDTO Create(CategoryDTO objDTO);
+        public CategoryDTO Update(CategoryDTO objDTO);
+        public CategoryDTO Get(int id);
+        public IEnumerable<CategoryDTO> GetAll();
+        public int Delete(int id);
+    }
+}
