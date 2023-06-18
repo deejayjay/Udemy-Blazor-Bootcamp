6983077f615ad4110fa2cb465394e3d49a1182b1
DeeJayJay <deejayjay@zoho.com>
2023-06-17 18:15:19 -0600

    Completes Section 15

--- TangyWeb_Client/Program.cs
+++ TangyWeb_Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TangyWeb_Client;
@@ -14,5 +15,9 @@ builder.Services.AddScoped(sp => new HttpClient
 });
 
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICartService, CartService>();
+
+// Add Blazored Local Storage to DI container
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
---
+++ TangyWeb_Client/Service/CartService.cs
@@ -0,0 +1,76 @@
+﻿using Blazored.LocalStorage;
+using Tangy_Common;
+using TangyWeb_Client.Service.IService;
+using TangyWeb_Client.ViewModels;
+
+namespace TangyWeb_Client.Service
+{
+    public class CartService : ICartService
+    {
+        private readonly ILocalStorageService _localStorage;
+        public CartService(ILocalStorageService localStorageService)
+        {
+            _localStorage = localStorageService;
+        }
+
+        public event Action? OnChange;
+
+        public async Task DecrementCart(ShoppingCart cartToDecrement)
+        {
+            // Get all existing carts from local storage.
+            var cart = await _localStorage.GetItemAsync<List<ShoppingCart>>(SD.ShoppingCart);
+            
+            for (int i = 0; i < cart.Count; i++) 
+            {
+                if (cart[i].ProductId == cartToDecrement.ProductId 
+                    && cart[i].ProductPriceId == cartToDecrement.ProductPriceId)
+                {
+                    if (cart[i].Count == 1 || cartToDecrement.Count == 0)
+                    {
+                        cart.Remove(cart[i]);
+                    }
+                    else
+                    {
+                        cart[i].Count -= cartToDecrement.Count;
+                    }
+                }
+            }
+
+            await _localStorage.SetItemAsync<List<ShoppingCart>>(SD.ShoppingCart, cart);
+            OnChange?.Invoke();
+        }
+
+        public async Task IncrementCart(ShoppingCart cartToAdd)
+        {
+            // Get all existing carts from local storage.
+            var cart = await _localStorage.GetItemAsync<List<ShoppingCart>>(SD.ShoppingCart);
+            bool itemInCart = false;
+
+            cart ??= new List<ShoppingCart>();
+
+            // If item already exists in the cart, increment count for that record.
+            foreach (var item in cart) 
+            {
+                if (item.ProductId == cartToAdd.ProductId && item.ProductPriceId == cartToAdd.ProductPriceId)
+                {
+                    itemInCart = true;
+                    item.Count += cartToAdd.Count;
+                }
+            }
+
+            // If item does not exist in the cart, add a new record to the cart.
+            if (!itemInCart) 
+            {
+                cart.Add(new ShoppingCart 
+                { 
+                    ProductId = cartToAdd.ProductId,
+                    ProductPriceId = cartToAdd.ProductPriceId,
+                    Count = cartToAdd.Count
+                });
+            }
+
+            await _localStorage.SetItemAsync<List<ShoppingCart>>(SD.ShoppingCart, cart);
+            OnChange?.Invoke();
+        }
+    }
+}
